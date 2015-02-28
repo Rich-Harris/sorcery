48677f39b68104d982e4cdd9bcce69760dbdd187
Rich-Harris <richard.a.harris@gmail.com>
2015-02-28 12:21:09 -0500

    speed up encodeMappings

--- src/utils/encodeMappings.js
+++ src/utils/encodeMappings.js
@@ -1,46 +1,96 @@
 import * as vlq from 'vlq';
 
+function handleLine ( line, offsets ) {
+	var generatedCodeColumn = 0;
+
+	let numSegments = line.length;
+	let encoded = new Array( numSegments );
+
+	let i, segment, encodedSegment;
+
+	for ( i = 0; i < numSegments; i += 1 ) {
+		segment = line[i];
+
+		if ( !segment.length ) {
+			encoded[i] = segment;
+			return;
+		}
+
+		encodedSegment = [ segment[0] - generatedCodeColumn ];
+		generatedCodeColumn = segment[0];
+
+		if ( segment.length === 1 ) {
+			// only one field!
+			return encodedSegment;
+		}
+
+		encodedSegment[1] = segment[1] - offsets.sourceFileIndex;
+		encodedSegment[2] = segment[2] - offsets.sourceCodeLine;
+		encodedSegment[3] = segment[3] - offsets.sourceCodeColumn;
+
+		offsets.sourceFileIndex  = segment[1];
+		offsets.sourceCodeLine   = segment[2];
+		offsets.sourceCodeColumn = segment[3];
+
+		if ( segment.length === 5 ) {
+			encodedSegment[4] = segment[4] - offsets.nameIndex;
+			offsets.nameIndex = segment[4];
+		}
+
+		encoded[i] = vlq.encode( encodedSegment );
+	}
+
+	return encoded.join( ',' );
+
+	/*return line.map( function ( segment ) {
+		var result;
+
+		if ( !segment.length ) {
+			return segment;
+		}
+
+		result = [ segment[0] - generatedCodeColumn ];
+		generatedCodeColumn = segment[0];
+
+		if ( segment.length === 1 ) {
+			// only one field!
+			return result;
+		}
+
+		result[1] = segment[1] - offsets.sourceFileIndex;
+		result[2] = segment[2] - offsets.sourceCodeLine;
+		result[3] = segment[3] - offsets.sourceCodeColumn;
+
+		offsets.sourceFileIndex  = segment[1];
+		offsets.sourceCodeLine   = segment[2];
+		offsets.sourceCodeColumn = segment[3];
+
+		if ( segment.length === 5 ) {
+			result[4] = segment[4] - offsets.nameIndex;
+			offsets.nameIndex = segment[4];
+		}
+
+		return vlq.encode( result );
+	}).join( ',' );*/
+}
+
 export default function encodeMappings ( decoded ) {
-	var mappings,
-		sourceFileIndex = 0,   // second field
-		sourceCodeLine = 0,    // third field
-		sourceCodeColumn = 0,  // fourth field
-		nameIndex = 0;         // fifth field
+	let offsets = {
+		sourceFileIndex: 0,    // second field
+		sourceCodeLine: 0,     // third field
+		sourceCodeColumn: 0,   // fourth field
+		nameIndex: 0           // fifth field
+	};
 
-	mappings = decoded.map( function ( line ) {
-		var generatedCodeColumn = 0; // first field - reset each time
+	let numLines = decoded.length;
+	let mappings = new Array( numLines );
 
-		return line.map( function ( segment ) {
-			var result;
+	let i, line;
 
-			if ( !segment.length ) {
-				return segment;
-			}
+	for ( i = 0; i < numLines; i += 1 ) {
+		line = decoded[i];
+		mappings[i] = handleLine( line, offsets );
+	}
 
-			result = [ segment[0] - generatedCodeColumn ];
-			generatedCodeColumn = segment[0];
-
-			if ( segment.length === 1 ) {
-				// only one field!
-				return result;
-			}
-
-			result[1] = segment[1] - sourceFileIndex;
-			result[2] = segment[2] - sourceCodeLine;
-			result[3] = segment[3] - sourceCodeColumn;
-
-			sourceFileIndex  = segment[1];
-			sourceCodeLine   = segment[2];
-			sourceCodeColumn = segment[3];
-
-			if ( segment.length === 5 ) {
-				result[4] = segment[4] - nameIndex;
-				nameIndex = segment[4];
-			}
-
-			return vlq.encode( result );
-		}).join( ',' );
-	}).join( ';' );
-
-	return mappings;
+	return mappings.join( ';' );
 }
