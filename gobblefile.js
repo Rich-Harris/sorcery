b5bea49007c4433cee9a2f655e1448f46b7c4f32
Rich-Harris <richard.a.harris@gmail.com>
2015-05-10 12:41:19 -0400

    build with sourcemaps

--- gobblefile.js
+++ gobblefile.js
@@ -3,25 +3,24 @@ var gobble = require( 'gobble' );
 gobble.cwd( __dirname );
 
 module.exports = gobble( 'src' )
-.transform( 'babel', {
-	whitelist: [
-		'es6.arrowFunctions',
-		'es6.blockScoping',
-		'es6.constants',
-		'es6.destructuring',
-		'es6.parameters.default',
-		'es6.parameters.rest',
-		'es6.properties.shorthand',
-		'es6.templateLiterals',
-		'es6.classes'
-	],
-	loose: [ 'es6.classes' ],
-	sourceMap: false
-})
-.transform( 'esperanto-bundle', {
-	entry: 'index',
-	dest: 'sorcery',
-	type: 'cjs',
-	strict: true,
-	sourceMap: false
-});
+	.transform( 'babel', {
+		whitelist: [
+			'es6.arrowFunctions',
+			'es6.blockScoping',
+			'es6.constants',
+			'es6.destructuring',
+			'es6.parameters.default',
+			'es6.parameters.rest',
+			'es6.properties.shorthand',
+			'es6.templateLiterals',
+			'es6.classes'
+		],
+		loose: [ 'es6.classes' ],
+		sourceMap: true
+	})
+	.transform( 'esperanto-bundle', {
+		entry: 'index',
+		dest: 'sorcery',
+		type: 'cjs',
+		strict: true
+	});
